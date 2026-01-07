d9a91ac5ad88ee2bb7ad0f80e48a5e11f011c0e6
felixon88 <incaqwf6@amway.com>
2026-01-08 00:35:18 +0530

    second commit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from EKS Python App!"
+    return "Hello from EKS Python App!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
